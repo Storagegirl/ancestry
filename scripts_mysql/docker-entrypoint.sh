a2fd16773f6eccb60ab6d5e4990505efcdea69a2
nicole.lang@storagegirl.de <nlang@snoopy.local>
2018-07-19 14:27:08 +0200

    secind step

--- scripts_mysql/docker-entrypoint.sh
+++ scripts_mysql/docker-entrypoint.sh
@@ -6,7 +6,7 @@ database=ancestry
 
 mysql --user="$user" --password="$password" --database="$database" --execute="CREATE TABLE members (name VARCHAR(35), sex CHAR(1), birth DATE, death DATE)"
 
-mysql --user="$user" --password="$password" --database="$database" --execute="INSERT INTO members VALUES ('Nicole Lang', 'f', '1983-11-21', NULL)"
+mysql --user="$user" --password="$password" --database="$database" --execute="INSERT INTO members VALUES ('Fritzi Fummel', 'm', '1964-08-11', NULL)"
 
 
 
